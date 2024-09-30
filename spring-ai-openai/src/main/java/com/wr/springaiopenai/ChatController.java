f62d73228eebe3debdeb1e9012d3832ae8955536
wrozwal <wrozwal@jellytech.com.pl>
2024-09-30 23:14:54 +0200

    change to 4o-mini model, add service and controller for openAI

---
+++ spring-ai-openai/src/main/java/com/wr/springaiopenai/ChatController.java
@@ -0,0 +1,23 @@
+package com.wr.springaiopenai;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    public final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping("/ask")
+    public String ask(@RequestParam(value = "query") String query) {
+        return chatService.ask(query);
+    }
+
+}
---
+++ spring-ai-openai/src/main/java/com/wr/springaiopenai/ChatService.java
@@ -0,0 +1,39 @@
+package com.wr.springaiopenai;
+
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    private final OpenAiChatModel chatModel;
+
+    public ChatService(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String ask(String query) {
+        String template = """
+                Masz tutaj czesc instukcji:
+                Cennik staje się cennikiem wygasłem w sytuacji, gdy upłynie okres ważności cennika wskazany w
+                polach Ważny od oraz Ważny do. Cennik można również wygasić z dniem bieżącym za pomocą opcji
+                Zakończ dzisiaj. Taki cennik stanie się wygasły tj. przeterminowany, ale wciąż pozostanie aktywny.
+                Aby zmienić status cennika na nieaktywny należy użyć opcji Dezaktywuj.
+                
+                Na jej podstawie instrukcji odpowiedz na pytanie:
+                {query} ?
+                Jeżeli pytanie dotyczy czegoś innego niż dana instrukcja to przekaż że nie masz takich danych w swojej instrukcji!
+                """;
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        promptTemplate.add("query", query);
+        Prompt prompt = promptTemplate.create();
+
+        return chatModel.call(prompt)
+                .getResult()
+                .getOutput()
+                .getContent();
+    }
+
+}
